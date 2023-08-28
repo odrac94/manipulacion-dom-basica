2cae0d64872c28247445955dfc60f5bc76e2851a
Ricardo Delfin <richardo.delfin94@gmail.com>
2023-08-28 13:58:36 -0700

    Agregar .gitignore y primeros archivos del proyecto

---
+++ main.js
@@ -0,0 +1,33 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+btn.addEventListener('click', sumarInputValues);
+
+// form.addEventListener('submit', sumarInputValues);
+
+
+function sumarInputValues(event) {
+    console.log({event});
+    event.preventDefault();
+    const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
+// function btnOnClickResta() {
+//     const sumaInputs = parseInt(input1.value) - parseInt(input2.value);
+//     pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+// function btnOnClickMulti() {
+//     const sumaInputs = parseInt(input1.value) * parseInt(input2.value);
+//     pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+// function btnOnClickDivision() {
+//     const sumaInputs = parseInt(input1.value) / parseInt(input2.value);
+//     pResult.innerText = "Resultado: " + sumaInputs;
+// }
